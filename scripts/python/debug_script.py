0ca624bca62540a25d12b36cfb322069e0d7b5e8
jtomori <tomori.juraj@gmail.com>
2018-06-13 14:15:32 +0200

    made it python3 compatible - for Blender

--- scripts/python/debug_script.py
+++ scripts/python/debug_script.py
@@ -3,9 +3,9 @@ import ptvsd
 # allow other computers to attach to ptvsd at this IP address and port, using the password
 try:
     ptvsd.enable_attach("SFds_KjLDFJ:LK", address = ('localhost', 3000))
-    print "Not attached already, attaching..."
+    print("Not attached already, attaching...")
 except ptvsd.AttachAlreadyEnabledError:
-    print "Attached already, continuing..."
+    print("Attached already, continuing...")
 
 
 def test():
@@ -18,5 +18,5 @@ def test():
     before = "before"
     after = "after"
 
-    print before
-    print after
+    print(before)
+    print(after)
